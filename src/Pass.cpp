d70c12380370b0b830b52ba5caa48e88c63a834c
roboguy13 <roboguy06@gmail.com>
2021-02-17 16:17:06 -0600

    Detect annotations

--- src/Pass.cpp
+++ src/Pass.cpp
@@ -3,6 +3,8 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Instructions.h"
 using namespace llvm;
 
 namespace {
@@ -12,6 +14,20 @@ namespace {
 
     virtual bool runOnFunction(Function& F) {
       errs() << "I saw a function called " << F.getName() << "!\n";
+      errs() << "Function body:\n" << F << "\n";
+      for (auto& B : F) {
+        errs() << "Basic block:\n" << B << "\n";
+        for (auto& I : B) {
+          errs() << "Instruction: " << I << "\n";
+          auto fnCall = dyn_cast<CallInst>(&I);
+          if (fnCall) {
+            errs() << "*** CallInst with name: " << fnCall->getCalledFunction()->getName() << "\n";
+            if (fnCall->getCalledFunction()->getName().equals("llvm.var.annotation")) {
+              errs() << "+++ Annotation call\n";
+            }
+          }
+        }
+      }
       return false;
     }
   };
--- test.c
+++ test.c
@@ -1,3 +1,11 @@
 char f(int x) { return 'a'; }
-int g(int y) { return y*2; }
+
+int g(int y) {
+  __attribute__((annotate("nospec"))) int z;
+  return y*2;
+}
+
+int main() {
+  return g(3);
+}
 
