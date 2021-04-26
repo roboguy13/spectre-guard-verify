41f46dfcc5fb2610818fcfc81f601cd0c28eddbf
David Young <roboguy06@gmail.com>
2021-04-26 00:20:02 -0500

    Use union-find to keep track of merging entries and merging exists in visualization code

--- src/DOT.hs
+++ src/DOT.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
 
 module DOT (genDOT, genDOT', DOTConfig(..), defaultDOTConfig) where
 
@@ -9,21 +11,63 @@ import           Data.Foldable
 import           Data.Set (Set)
 import qualified Data.Set as Set
 
+import           Data.List
+
+-- import           Data.Equivalence.Monad
+import           Data.Equivalence.STT --(Equiv, leastEquiv)
+import           Data.Functor.Identity
+import           Control.Monad.Reader
+import           Control.Monad.ST.Trans
+
+type DOTEquiv s = Equiv s [NodeId] NodeId
+
+newtype DOTM s a = DOTM { unDOTM :: ReaderT (DOTEquiv s, DOTEquiv s) (STT s Identity) a }
+  deriving (Functor, Applicative, Monad, MonadReader (DOTEquiv s, DOTEquiv s))
+
+entryEquiv :: DOTM s (DOTEquiv s)
+entryEquiv = fst <$> ask
+
+exitEquiv :: DOTM s (DOTEquiv s)
+exitEquiv = snd <$> ask
+
+-- onEntryEquiv :: (DOTEquiv s -> r) -> DOTM s r
+-- onEntryEquiv f =
+--   fmap f entryEquiv
+
+liftSTT :: STT s Identity a -> DOTM s a
+liftSTT s = DOTM (lift s)
+
+runDOTM :: (forall s. DOTM s a) -> a
+runDOTM m = runIdentity $ runSTT $ do
+  entryEq <- leastEquiv (:[]) (++)
+  exitEq <- leastEquiv (:[]) (++)
+
+  runReaderT (unDOTM m) (entryEq, exitEq)
+
+fastNub :: Ord a => [a] -> [a]
+fastNub = Set.toList . Set.fromList
 
 genDOT' :: SetConstraints -> String
 genDOT' = genDOT defaultDOTConfig
 
 genDOT :: DOTConfig -> SetConstraints -> String
 genDOT config constraints =
-  unlines
-    [ "digraph {"
-    , indent (genConfig config)
-    , indent (toList (foldr (<>) mempty (map genBoundaries constraints)))
-    -- , indent (genEntries constraints)
-    -- , indent (genExits constraints)
-    , indent (concatMap (uncurry genDOTFor) (zip [1..] constraints))
-    , "}"
-    ]
+  runDOTM $ do
+    mapM_ combineEntries constraints
+    mapM_ combineExits constraints
+    boundaries <- mapM genBoundaries constraints
+
+    connections <- mapM (uncurry genDOTFor) (zip [1..] constraints)
+
+    return $ unlines
+      [ "strict digraph {"
+      , indent (genConfig config)
+      , indent (fastNub (concat boundaries))
+      -- , indent (genEntries constraints)
+      -- , indent (genExits constraints)
+      , indent (concat connections)
+      , "}"
+      ]
 
 indent :: [String] -> String
 indent = unlines . map ("  " <>)
@@ -44,7 +88,7 @@ defaultDOTConfig =
   , nodeColor = "white"
   , nodeFontColor = "white"
   , edgeColor = "white"
-  , graphMinSize = 45
+  , graphMinSize = 25
   }
 
 genConfig :: DOTConfig -> [String]
@@ -54,12 +98,15 @@ genConfig DOTConfig {..} =
   , "edge [color=" <> edgeColor <> "]"
   ]
 
-genDOTFor :: Int -> SetConstraint -> [String]
-genDOTFor graphN sc =
-  [ "subgraph " <> show graphN <> " {"
-  , indent (genDOTFor' sc)
-  , "}"
-  ]
+genDOTFor :: Int -> SetConstraint -> DOTM s [String]
+genDOTFor graphN sc = do
+  r <- genDOTFor' sc
+
+  return
+    [ "subgraph " <> show graphN <> " {"
+    , indent r
+    , "}"
+    ]
 
 data Arrowhead = Forward | Backward
 
@@ -71,7 +118,9 @@ dotConnect :: NodeId -> NodeId -> String
 dotConnect (NodeId x) (NodeId y) = dotConnect' (show x) (show y)
 
 dotConnect' :: String -> String -> String
-dotConnect' x y = x <> " -> " <> y <> ";"
+dotConnect' x y
+  | x /= y = x <> " -> " <> y <> ";"
+  | x == y = ""
 
 class BoundaryNodes a where
   getEntryNodes :: a -> Set NodeId
@@ -113,36 +162,87 @@ exit (NodeId n) = "exit" <> show n
 node :: NodeId -> String
 node (NodeId n) = "n" <> show n
 
-genBoundaries :: SetConstraint -> Set String
-genBoundaries (x :=: y) =
-  let allNodeIds = getNodeIds x <> getNodeIds y
-      entries = getEntryNodes x <> getEntryNodes y
-      exits = getExitNodes x <> getExitNodes y
-  in
-    foldr (<>) mempty $
-    flip Set.map allNodeIds
-      $ \n ->
-          Set.map (`dotConnect'` (node n)) (Set.map entry entries)
-          <> Set.map (dotConnect' (node n)) (Set.map exit exits)
-          <> Set.singleton (entry n <> " [shape=box];")
-          <> Set.singleton (exit n <> " [shape=box];")
+nodeClassName :: (NodeId -> String) -> [NodeId] -> String
+nodeClassName f = intercalate "_" . map f
 
+genBoundaries :: SetConstraint -> DOTM s [String]
+genBoundaries (x :=: y) = do
+  let allNodeIds = toList $ getNodeIds x <> getNodeIds y
+      entries0 = toList $ getEntryNodes x <> getEntryNodes y
+      exits0 = toList $ getExitNodes x <> getExitNodes y
 
+  entryEq <- entryEquiv
+  exitEq <- exitEquiv
 
-genConnections :: (String -> String -> String) -> String -> SetExpr freeVars -> [String]
-genConnections connect endpointA e =
-  let entries = toList $ getEntryNodes e
-      exits = toList $ getExitNodes e
-  in
-  map (connect endpointA) (map entry entries)
-  ++ map (connect endpointA) (map exit exits)
+  entries <- liftSTT $ map (nodeClassName entry) <$> mapM (classDesc entryEq) entries0
+  exits   <- liftSTT $ map (nodeClassName exit)  <$> mapM (classDesc exitEq) exits0
 
-genDOTFor' :: SetConstraint -> [String]
--- genDOTFor' = undefined
--- -- genDOTFor' (C_Entry' n :=: y) = 
-genDOTFor' (C_Entry' n :=: y) = genConnections (flip dotConnect') (entry n) y
-genDOTFor' (C_Exit'  n :=: y) = genConnections dotConnect' (exit n) y
--- genDOTFor' (Atom_E'  n :=: y) = map (dotConnect n) (toList (getNodeIds y))
-genDOTFor' (Atom_E' {} :=: _) = []
-genDOTFor' (Atom_S' {} :=: _) = []
+  return $
+    fmap concat $ flip mapM allNodeIds
+        $ \n -> do
+            -- theEntry <-
+            map (`dotConnect'` (node n)) entries
+            <> map (dotConnect' (node n)) exits
+            <> map (<> " [shape=box];") (entries ++ exits)
+            -- <> [entry n <> " [shape=box];"]
+            -- <> [exit n <> " [shape=box];"]
+
+  -- return $ foldr (<>) mempty $
+  --   flip Set.map allNodeIds
+  --     $ \n ->
+  --         Set.map (`dotConnect'` (node n)) (Set.map entry entries)
+  --         <> Set.map (dotConnect' (node n)) (Set.map exit exits)
+  --         <> Set.singleton (entry n <> " [shape=box];")
+  --         <> Set.singleton (exit n <> " [shape=box];")
+
+combineEntries :: SetConstraint -> DOTM s ()
+combineEntries (C_Entry' n :=: SE_Atom (C_Entry m)) = do
+  entryEq <- entryEquiv
+
+  nClass <- liftSTT $ getClass entryEq n
+  mClass <- liftSTT $ getClass entryEq m
+  liftSTT $ combine entryEq nClass mClass
+  return ()
+combineEntries _ = return ()
+
+combineExits :: SetConstraint -> DOTM s ()
+combineExits (C_Exit' n :=: SE_Atom (C_Exit m)) =  do
+  exitEq <- exitEquiv
+
+  nClass <- liftSTT $ getClass exitEq n
+  mClass <- liftSTT $ getClass exitEq m
+  liftSTT $ combine exitEq nClass mClass
+  return ()
+combineExits _ = return ()
+
+genConnections :: (String -> String -> String) -> String -> SetExpr freeVars -> DOTM s [String]
+genConnections connect endpointA e = do
+  let entries0 = toList $ getEntryNodes e
+      exits0 = toList $ getExitNodes e
+
+  entryEq <- entryEquiv
+  exitEq <- exitEquiv
+
+  let classDescEntry = classDesc entryEq
+      classDescExit  = classDesc exitEq
+
+  entries <- liftSTT $ map (nodeClassName entry) <$> mapM classDescEntry entries0
+  exits   <- liftSTT $ map (nodeClassName exit)  <$> mapM classDescExit exits0
+
+  return $ map (connect endpointA) entries
+           ++ map (connect endpointA) exits
+
+genDOTFor' :: SetConstraint -> DOTM s [String]
+genDOTFor' (C_Entry' n :=: y) = do
+  entryEq <- entryEquiv
+  nDesc <- liftSTT $ classDesc entryEq n
+  genConnections (flip dotConnect') (nodeClassName entry nDesc) y
+
+genDOTFor' (C_Exit'  n :=: y) = do
+  exitEq <- exitEquiv
+  nDesc <- liftSTT $ classDesc exitEq n
+
+  genConnections dotConnect' (nodeClassName exit nDesc) y
+genDOTFor' (Atom_E' {} :=: _) = return []
+genDOTFor' (Atom_S' {} :=: _) = return []
 
